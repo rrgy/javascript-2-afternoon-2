9f61e9922f7d32b4c5833ba175d46a07f2b8ec65
Ryan Morgan <rrgy80@gmail.com>
2019-08-15 00:03:00 -0600

    still working on

--- practice.js
+++ practice.js
@@ -17,7 +17,9 @@ var arr = [10,20,30];
   Return the first item in the given array.
 */
 
-//Code Here
+function first(arr){
+  return arr[0]
+}
 
 
 
@@ -32,7 +34,8 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
+function last(arr){
+  return arr[2]}
 
 
 
@@ -47,7 +50,13 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
+function looper(family){
+  for (let i = 0; i < family.length; i++){
+    alert (family[i])
+  }
+}
+looper(family)
+
 
 
 
@@ -62,7 +71,7 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the letters array, alerting every item in the array.
 */
 
-//Code Here
+function reversedLooper(letters){}
 
 
 
